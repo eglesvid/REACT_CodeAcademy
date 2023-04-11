6df630c2d908d7cb91da19d3fca0471321db3654
eglesvid <eglesvid@gmail.com>
2023-04-11 22:04:44 +0300

    delete

--- 17. Pamoka Render Props_HOC/task3/src/components/Fahrenheit/Fahrenheit.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import "../../App.css";
-
-export default function Fahrenheit({ value }) {
-  return <div className="temp">{(parseInt(value || 0) * 9) / 5 + 32}°F</div>;
-}
--- 17. Pamoka Render Props_HOC/task3/src/components/Kelvin/Kelvin.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import "../../App.css";
-
-export default function Kelvin({ value }) {
-  return <div className="temp">{parseInt(value || 0) + 273.15}K</div>;
-}
